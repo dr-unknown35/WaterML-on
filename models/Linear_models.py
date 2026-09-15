7e369edb10f015f61fa402df0ab999d9ecf2d6f7
dr-unknown35 <dr.unknown999@gmail.com>
2026-09-15 22:29:27 +0100

    feat: implement LocalWeightedLR with dedicated closed-form solver

--- models/Linear_models.py
+++ models/Linear_models.py
@@ -1,7 +1,7 @@
 from .base import BaseEstimator
-from ..optim.gradient_methods import GradientDescent , NormalEquations, NewtonsMethod
+from ..optim.gradient_methods import GradientDescent , NormalEquations, NewtonsMethod,LocalWeightedSolver
 from .families import Gaussian, Binomial , Poisson , Gamma
-
+import numpy as np
 
 class GLM(BaseEstimator):
     def __init__(self, family=None, solver=None ,learning_rate=0.01,epochs=2000, penalty=None, alpha=0.1):
@@ -16,7 +16,7 @@ class GLM(BaseEstimator):
         
 
     def fit(self,X,y):
-        self.weights,self.bias=self.solver.optimize(self,X,y)
+        self.bias,self.weights=self.solver.optimize(self,X,y)
         return self
     
     
@@ -28,10 +28,51 @@ class GLM(BaseEstimator):
         return (self.predict(X)>=threshold).astype(int)
 
 
-        
+
+class LocalWeightedLR(BaseEstimator):
+    def __init__(self,tau=1,solver=None,family=None):
+        self.tau=tau
+        self.solver=solver if solver !=None else LocalWeightedSolver()
+        self.family=family if family !=None else Gaussian()
+        self.X=None
+        self.y=None
+        self.weights=None
+        self.bias=None
+
+
+    def fit(self,X,y):
+        self.X=X
+        self.y=y
+        return self
+
+
+    def predict(self,test_X):
+        pred=[]
+        for row in test_X:
+            self.bias,self.weights=self.solver.optimize_local(self, self.X, self.y, row)
+            pred.append(self.family.inverse_link(row@self.weights +self.bias))
+        return pred
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class LinearRegression(GLM):
     def __init__(self,solver=NormalEquations()):
         super().__init__(family=Gaussian(),solver=solver)
+
 
 class LogisticRegression(GLM):
     def __init__(self,solver=GradientDescent()):
